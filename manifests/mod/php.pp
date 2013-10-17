683adb5c2d31ef4cee94d6055f727f9803a672ee
Daniel Beckham <dbeckham@dealnews.com>
2013-10-17 17:08:22 -0500

    Added support for custom PHP handler extensions; updated config for mod_php
    Traditionally .php, .phtml, .inc, and even .html have been used for PHP
    scripts.  This change adds support for mod_php handling more than just the
    .php file extension.
    
    Other Changes:
    * Added proper AddHander, AddType support for apache2.2+mod_php
    * Added support for additional PHP specific directory indexes
    * Added support for .phps source handler. (Disabled by default.)
    
    NOTE: This has not been fully tested with mod_fastcgi or a straight cgi
    implementation.

--- manifests/mod/php.pp
+++ manifests/mod/php.pp
@@ -1,5 +1,8 @@
 class apache::mod::php (
-  $package_ensure = 'present',
+  $package_ensure        = 'present',
+  $handler_extensions    = ['.php'],
+  $directory_indexes     = ['index.php'],
+  $enable_phps_extension = false,
 ) {
   if ! defined(Class['apache::mod::prefork']) {
     fail('apache::mod::php requires apache::mod::prefork; please enable mpm_module => \'prefork\' on Class[\'apache\']')
@@ -7,6 +10,7 @@ class apache::mod::php (
   apache::mod { 'php5':
     package_ensure => $package_ensure,
   }
+  # Template uses $handler_extensions, $directory_indexes, $enable_phps_extension
   file { 'php5.conf':
     ensure  => file,
     path    => "${apache::mod_dir}/php5.conf",
